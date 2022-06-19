0c16fc52fba5bc48266737abf409fc0519783fbc
Max Goodhart <c@chromakode.com>
2022-06-19 16:24:13 -0700

    Prevent cheeky prototype accesses in deepTemplate

--- src/utils/deepTemplate.tsx
+++ src/utils/deepTemplate.tsx
@@ -1,10 +1,20 @@
-import { get, isArray, isPlainObject, isString, map, mapValues } from 'lodash'
+import {
+  get,
+  has,
+  isArray,
+  isPlainObject,
+  isString,
+  map,
+  mapValues,
+} from 'lodash'
 
 const templateSubRe = /\$\{([\w.]+)\}/g
 
 export default function deepTemplate(obj: any, values: any): any {
   if (isString(obj)) {
-    return obj.replace(templateSubRe, (match, key) => get(values, key))
+    return obj.replace(templateSubRe, (match, key) =>
+      has(values, key) ? get(values, key) : undefined,
+    )
   } else if (isPlainObject(obj)) {
     return mapValues(obj, (v) => deepTemplate(v, values))
   } else if (isArray(obj)) {
