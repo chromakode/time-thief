2b4a0ea89d7c9cecadc3c252b157061394045552
Max Goodhart <c@chromakode.com>
2022-07-19 00:01:05 -0700

    Redirect to /app for now

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,9 +1,10 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import App from './App'
 
 export default function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/app" replace />} />
       <Route path="/app" element={<App />} />
       <Route path="/app/log" element={<App isShowingLog />} />
     </Routes>
